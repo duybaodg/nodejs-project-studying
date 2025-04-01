67e30d0d99dc636ccc40b1e860a78af26476b345
gitdev1 <gitdev1@gmail.com>
2025-04-02 11:22:43 +1300

    add controoller and routers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
@@ -23,23 +25,11 @@ app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 console.log("PATH: ", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/news", (req, res) => {
-  console.log(req.query.q);
-  res.render("news");
-});
-app.get("/search", (req, res) => {
-  console.log(req.query.q);
-  res.render("search");
-});
-app.post("/search", (req, res) => {
-  console.log(req.body.q);
-  //query parameter: .query
-  //form data: .body
-  res.send("");
-});
+//Home, search, contact
+
+//Routes init
+route(app);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
