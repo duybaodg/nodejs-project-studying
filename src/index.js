7b76d2e70178966d850350990955dd3bd74d6646
gitdev1 <gitdev1@gmail.com>
2025-04-02 15:19:22 +1300

    remove husky from package.json

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
 //XMLHTTPRequest, fetch, axios,
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 //template engine
-app.engine(".hbs", engine({ extname: ".hbs" }));
-app.set("view engine", ".hbs");
-app.set("views", path.join(__dirname, "resources/views"));
-console.log("PATH: ", path.join(__dirname, "resources/views"));
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+console.log('PATH: ', path.join(__dirname, 'resources/views'));
 
 //Home, search, contact
 
@@ -31,5 +31,5 @@ console.log("PATH: ", path.join(__dirname, "resources/views"));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
