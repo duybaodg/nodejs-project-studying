aa267a02d71f8c9f8daf0a3165d66403514b6a11
gitdev1 <gitdev1@gmail.com>
2025-04-04 16:12:22 +1300

    create trash function and soft delete

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -7,7 +7,9 @@ route.get('/create', courseController.create);
 route.post('/store', courseController.store);
 route.get('/:id/edit', courseController.edit);
 route.put('/:id', courseController.update);
+route.patch('/:id/restore', courseController.restore);
 route.delete('/:id', courseController.destroy);
+route.delete('/:id/force', courseController.forceDestroy);
 route.get('/:slug', courseController.show);
 
 module.exports = route;
--- src/routes/me.js
+++ src/routes/me.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const route = express.Router();
 
-const meController = require('../app/controllers/MeController');
+const meController = require('../app/controllers/MEController');
 
 route.get('/stored/courses', meController.storedCourses);
+route.get('/trash/courses', meController.trashCourses);
 
 module.exports = route;
